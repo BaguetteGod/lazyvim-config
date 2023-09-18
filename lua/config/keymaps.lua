e11542fcd30b460a6a872bccfd7d4c4d807ab5a8
baguettegod <mikavm@outlook.com>
2023-09-18 08:29:25 +0200

    Added two keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,4 +6,7 @@ vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
