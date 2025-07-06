b34a2a5e636ad4d00fcdf5450445a94635221c92
BaguetteGod <mikavm@outlook.com>
2025-07-06 13:23:38 +0200

    config

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,10 +1,5 @@
 return {
   "OXY2DEV/markview.nvim",
-  lazy = false, -- Recommended
-  -- ft = "markdown" -- If you decide to lazy-load anyway
-
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
-  },
+  priority = 49,
+  lazy = false,
 }
