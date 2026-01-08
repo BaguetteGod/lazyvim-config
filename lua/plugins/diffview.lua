cfcb923d79885d1d953632d42017c71c9e85f9d8
BaguetteGod <mikavm@outlook.com>
2026-01-08 19:25:21 +0100

    Added diffview

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,8 @@
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+    { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+    { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+  },
+}
